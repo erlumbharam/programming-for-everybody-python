e73067bcfe56ec4bd312ac23ee9354d0644ee380
LUMBHA RAM <63182965+erlumbharam@users.noreply.github.com>
2020-04-29 01:08:34 +0530

    Update ass-5.2.py

--- Week-5/ass-5.2.py
+++ Week-5/ass-5.2.py
@@ -1,26 +1,3 @@
-# Assignment 5.2 - Guilherme Ferreira
 
-largest = None
-smallest = None
-while True:
-    num = raw_input("Enter a number: ")
-    
-    if num == "done" : break
-    
-    try:
-        num = int(num)
-    except:
-        print "Invalid input"
-        continue
-    
-    if smallest is None : smallest = num
-        
-    if largest is None : largest = num
-   
-    if(num > largest) :	largest = num
-        
-    if(num < smallest) : smallest = num
-        
 
-print "Maximum is", largest
-print "Minimum is", smallest
+Check out my product on Instamojo:https://www.instamojo.com/rahuljodhpur/coursera-python-for-everybody-assignment-52--90dc8/
